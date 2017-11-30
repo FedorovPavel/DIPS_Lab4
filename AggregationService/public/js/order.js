c84bf795b8c05fa172b580f6d1af3e9315cbd3e9
Pavel <IU7.2@yandex.ru>
2017-11-30 21:06:41 +0300

    Изменение логики для заказов

--- AggregationService/public/js/order.js
+++ AggregationService/public/js/order.js
@@ -1,9 +1,17 @@
+// class order{
+//     constructor(){
+
+//     }
+// }
+
 var orderTemplate;
+var orderErrorTemplate;
 
 function getOrderTemplate(){
     const url = '/orderTemplate';
     $.get(url, function(template){
-        orderTemplate = $.parseHTML(template);
+        orderTemplate = $.parseHTML(template.successfully);
+        orderErrorTemplate = $.parseHTML(template.error);
     });
 }
 
@@ -11,7 +19,8 @@ function getOrders(page, count) {
     const url = '/aggregator/orders?page=' + page + '&count=' + count;
     $.get(url, function(res){
         clearList();
-        $('div#list').append(res.html);
+        $('div#list').attr('currentPage',page);
+        fillListWithOrder(res.content);
         pagination(res.info.current, res.info.pages);
     });
 }
