8e23a324ded061aae5f71d92c98c3d58e6188cab
Pavel <IU7.2@yandex.ru>
2017-12-01 01:48:14 +0300

    Переработан клиентский контроллер для автомобилей

--- AggregationService/public/js/car.js
+++ AggregationService/public/js/car.js
@@ -1,101 +1,96 @@
-// class car{
-//     constructor(){
-
-//     }
-// }
-
-var carTemplate;
-var carErrorTemplate;
-
-function getCarTemplate(){
-    const url = '/carTemplates';
-    $.get(url, function(template){
-        carTemplate = $.parseHTML(template.successfully);
-        carErrorTemplate = $.parseHTML(template.error);
-    });
-    return;
-}
-
-function fillListWithCar(list){
-    for (let I = 0; I < list.length; I++){
-        const content = list[I];
-        let record = $(carTemplate).clone();
-        $(record).attr('id', content.id);
-        $(record).find('div.record_header').attr('carId',content.id)
-                                                .attr('filling',false)
-                                                .attr('state','close');
-        $(record).find('div.record_header').click(function(){
-            if ($(this).attr('state') == 'close'){
-                if ($(this).attr('filling') == 'false'){
-                    getCar($(this).attr('carId'));
-                    $(this).attr('filling', true);
-                } else {
-                    $(record).find('.detail_info').removeClass('hidden');
-                }
-                $(this).find('.view_detail_info').removeClass('glyphicon-chevron-down');
-                $(this).find('.view_detail_info').addClass('glyphicon-chevron-up');
-                $(this).attr('state', 'open');
-            } else {
-                $(this).find('.view_detail_info').removeClass('glyphicon-chevron-up');
-                $(this).find('.view_detail_info').addClass('glyphicon-chevron-down');
-                $(record).find('.detail_info').addClass('hidden');
-                $(this).attr('state', 'close');
-            }
-        });
-        $(record).find('h4.title').text(content.Manufacturer + ' ' + content.Model);
-        $(record).find('span.manufacture').text(content.Manufacturer);
-        $(record).find('span.model').text(content.Model);
-        $(record).find('span.type').text(content.Type);
-        $(record).find('span.cost').text(content.Cost);
-        $(record).find('button.create_order').click(function(){
-            alert("Заказ");
-        });
-        $('div#list').append(record);
+class car{
+    constructor(refToMenu){
+        this.menu = refToMenu;
+        this.template;
+        this.getCarTemplate();
     }
-}
 
-function addToListException(err){
-    let template = $(carErrorTemplate).clone();
-    $(template).find('span.status_code').text(err.status);
-    $(template).find('span.error_msg').text(err.responseText);
-    $('div#list').append(template);
-    return;
-}
-
-function getCars(page, count) {
-    const url = '/aggregator/catalog?page=' + page + '&count=' + count;
-    $.get(url)
-        .done(function(res){
-            clearList();
-            $('div#list').attr('currentPage',page);
-            fillListWithCar(res.content);
-            pagination(res.info.current, res.info.pages);
-        })
-        .fail(function(res){
-            clearList();
-            $('div#list').attr('currentPage',0);
-            addToListException(res);
-            pagination(0,0);
-        })
-}
-
-function updateCarInfo(record, info){
-    const detail = $(record).find('.detail_info');
-    $(detail).find('span.door').text(info.Doors);
-    $(detail).find('span.person').text(info.Person);
-    $(detail).find('span.locationCity').text(info.Location.City);
-    $(detail).find('span.locationStreet').text(info.Location.Street);
-    $(detail).find('span.locationHouse').text(info.Location.House);
-    $(detail).removeClass('hidden');
-}
-
-function getCar(id) {
-    const url = '/aggregator/catalog/' + id;
-    $.get(url, function(res){
-        const record = $('div#list').find('div').filter(function(index){
-            if (id == $(this).attr('id'))
-                return true;
+    getCarTemplate(){
+        let self = this;
+        const url = '/carTemplates';
+        $.get(url, function(template){
+            self.template = $.parseHTML(template);
         });
-        updateCarInfo(record, res);
-    });
+        return;
+    }
+
+    fillListWithCar(list){
+        let self = this;
+        for (let I = 0; I < list.length; I++){
+            const content = list[I];
+            let record = $(self.template).clone();
+            $(record).attr('id', content.id);
+            $(record).find('div.record_header').attr('carId',content.id)
+                                                    .attr('filling',false)
+                                                    .attr('state','close');
+            $(record).find('div.record_header').click(function(){
+                if ($(this).attr('state') == 'close'){
+                    if ($(this).attr('filling') == 'false'){
+                        self.getCar($(this).attr('carId'));
+                        $(this).attr('filling', true);
+                    } else {
+                        $(record).find('.detail_info').removeClass('hidden');
+                    }
+                    $(this).find('.view_detail_info').removeClass('glyphicon-chevron-down');
+                    $(this).find('.view_detail_info').addClass('glyphicon-chevron-up');
+                    $(this).attr('state', 'open');
+                } else {
+                    $(this).find('.view_detail_info').removeClass('glyphicon-chevron-up');
+                    $(this).find('.view_detail_info').addClass('glyphicon-chevron-down');
+                    $(record).find('.detail_info').addClass('hidden');
+                    $(this).attr('state', 'close');
+                }
+            });
+            $(record).find('h4.title').text(content.Manufacturer + ' ' + content.Model);
+            $(record).find('span.manufacture').text(content.Manufacturer);
+            $(record).find('span.model').text(content.Model);
+            $(record).find('span.type').text(content.Type);
+            $(record).find('span.cost').text(content.Cost);
+            $(record).find('button.create_order').attr('carId', content.id);
+            $(record).find('button.create_order').click(function(sender){
+                let carId = $(this).attr('carId');
+                self.menu.createDraftOrder(content);
+            });
+            $(self.menu.getList()).append(record);
+        }
+    }
+    
+    getCars(page, count, bind) {
+        let self = bind;
+        const url = '/aggregator/catalog?page=' + page + '&count=' + count;
+        $.get(url)
+            .done(function(res){
+                self.menu.clearList();
+                self.fillListWithCar(res.content);
+                self.menu.pagination(res.info.current, res.info.pages);
+            })
+            .fail(function(res){
+                self.menu.clearList();
+                self.menu.page = 0;
+                self.menu.addToListException(res);
+                self.menu.pagination(0,0);
+            })
+    }
+    
+    updateCarInfo(record, info){
+        const detail = $(record).find('.detail_info');
+        $(detail).find('span.door').text(info.Doors);
+        $(detail).find('span.person').text(info.Person);
+        $(detail).find('span.locationCity').text(info.Location.City);
+        $(detail).find('span.locationStreet').text(info.Location.Street);
+        $(detail).find('span.locationHouse').text(info.Location.House);
+        $(detail).removeClass('hidden');
+    }
+    
+    getCar(id) {
+        let self = this;
+        const url = '/aggregator/catalog/' + id;
+        $.get(url, function(res){
+            const record = $('div#list').find('div').filter(function(index){
+                if (id == $(this).attr('id'))
+                    return true;
+            });
+            self.updateCarInfo(record, res);
+        });
+    }
 }
