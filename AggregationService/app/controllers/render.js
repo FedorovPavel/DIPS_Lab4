01318a0c994baf25caf865193b047e9ec9f30087
Pavel <IU7.2@yandex.ru>
2017-12-03 23:46:30 +0300

    Добавлен рендеринг шаблона оплаты

--- AggregationService/app/controllers/render.js
+++ AggregationService/app/controllers/render.js
@@ -7,8 +7,8 @@ const views_dir = './app/views/';
 const   car     = jade.compileFile(views_dir + 'car_template.jade'),
         err     = jade.compileFile(views_dir + 'error_template.jade'),
         ord     = jade.compileFile(views_dir + 'order_template.jade'),
-        draft   = jade.compileFile(views_dir + 'order_draft_template.jade');
-
+        draft   = jade.compileFile(views_dir + 'order_draft_template.jade'),
+        paid    = jade.compileFile(views_dir + 'order_paid_template.jade');
 
 module.exports = function (app) {
     app.use('/', router);
@@ -32,4 +32,8 @@ router.get('/orderTemplate',function(req, res, next){
 
 router.get('/orderTemplate/draft',function(req, res, next){
     res.status(200).send(draft());
+});
+
+router.get('/orderTemplate/paid', function(req, res, next){
+    res.status(200).send(paid());
 });
